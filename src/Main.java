4f84c4fde07ae5e3d9f39e1dc53154b3bc61e7f9
Ivan-Castill <ivan2003castillo12345@gmail.com>
2024-10-16 21:37:07 -0500

    Tarea_S2_Ejercicio02_LosEmpleados

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+class Empleado {
+    // Atributos
+    private String nombre;
+    private String cargo;
+    private double salario;
+    private String fechaIngreso;
+    public Empleado(String nombre, String cargo, double salario, String fechaIngreso) {
+        this.nombre = nombre;
+        this.cargo = cargo;
+        this.salario = salario;
+        this.fechaIngreso = fechaIngreso;
+    }
+    //mostrar la información del empleado
+    public void mostrarInformacion() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Cargo: " + cargo);
+        System.out.println("Salario: $" + salario);
+        System.out.println("Fecha de ingreso: " + fechaIngreso);
+        System.out.println();
     }
 }
+public class Main {
+    public static void main(String[] args) {
+        //datos quemados
+        Empleado empleado1 = new Empleado("Juan Pérez", "Gerente", 2500.50, "01/03/2020");
+        Empleado empleado2 = new Empleado("María López", "Contadora", 1800.75, "12/11/2019");
+        Empleado empleado3 = new Empleado("Carlos García", "Ingeniero", 2000.00, "15/08/2018");
+        //mostrar la información de los empleados
+        System.out.println("Información de los empleados:");
+        empleado1.mostrarInformacion();
+        empleado2.mostrarInformacion();
+        empleado3.mostrarInformacion();
+    }
+}
+
